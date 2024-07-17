c9593d17a4e1886683c55c2bde0592361a03f0ea
prabhakar <prabhakar.devstack@gmail.com>
2024-07-17 14:51:59 +0530

    1st and 2nd project - completed, 3rd - in progress

---
+++ story/java-script/3rd-project/project/texting.js
@@ -0,0 +1,71 @@
+
+
+
+/*
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+console.log("numberOfDrumButtons:"+ numberOfDrumButtons);
+for(var i=0; i<numberOfDrumButtons; i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        this.style.color =" white ";
+    });
+}
+*/
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+console.log("numberOfDrumButtons:"+ numberOfDrumButtons);
+for(var i=0; i<numberOfDrumButtons; i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        this.style.color =" white ";
+        var buttonInnerHTML = this.innerHTML;
+       makesound(buttonInnerHTML);
+
+    });
+} 
+
+document.addEventListener("keypress",function(event){
+    /*      alert("key was pressed!");      */
+    makesound(event.key);
+});
+function makesound(key) {
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sound/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio("sound/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("sound/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("sound/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var crash = new Audio("sound/crash.mp3");
+            crash.play();
+            break; 
+        case "k":
+            var snare = new Audio("sound/snare.mp3");
+            snare.play();
+            break;
+        case "l":
+            var kick = new Audio("sound/kick-bass.mp3");
+            kick.play();
+            break;    
+       
+        default: console.log(buttonInnerHTML);
+       }
+    
+}
+
+/*
+document.querySelector(".drum").addEventListener("click",clickhandler);
+function clickhandler() {
+    alert("i am back");
+}
+  */ 
---
+++ story/java-script/advance javascript/constructor.js
@@ -0,0 +1,8 @@
+function Housekeeper(name, age, permition, Language ){
+    this.name = name;
+    this.age = age;
+    this.permition = permition;
+    this.language;
+}
+var Housekeeper1 =new Housekeeper("ram","32","yes","english");
+var Housekeeper2 =new Housekeeper("mohan","42","yes","english,franch");
