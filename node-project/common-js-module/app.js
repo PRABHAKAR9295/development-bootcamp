41b484ec51a204497a9821f3522dbba67185f834
prabhakar <prabhakar.devstack@gmail.com>
2024-08-07 17:47:09 +0530

    Node - Common Js Module

---
+++ node-project/common-js-module/app.js
@@ -0,0 +1,9 @@
+const math = require("./math")
+
+// console.log(math)
+
+let addResult = math.add(4,5);
+let subResult = math.subtract(2,3);
+
+// console.clear();
+console.log("add: "+addResult, "sub: "+subResult);
