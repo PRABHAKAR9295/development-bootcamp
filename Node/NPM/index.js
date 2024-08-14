95e4254999c24c638ae80f7729f7d25d34be5e67
prabhakar <prabhakar.devstack@gmail.com>
2024-08-14 11:02:25 +0530

    add file

---
+++ Node/NPM/index.js
@@ -0,0 +1,22 @@
+/*var generateName = require('sillyname');
+
+ import generateName from "sillyName";
+   var sillyName = generateName();
+
+   console.log(`My Name is ${sillyName}.`);
+*/
+
+/*
+// use to superherose module
+// const superheroes = require("superheroes");
+ import superheroes from "superheroes";
+
+ const name = superheroes;
+ console.log(`I am ${name}!`);
+*/
+
+import {randomSuperhero} from 'superheroes';
+
+const name = randomSuperhero();
+console.log(`I am ${name}!`);
+
